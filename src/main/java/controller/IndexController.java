30ecd8e913b22e9f2e31dda4e01c1de80613b764
doSMgochi <aenarchi@gmail.com>
2024-06-13 14:09:06 +0900

    first commit

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class IndexController extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setAttribute("visit", "health check");
+		
+		request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
+		
+	}
+}
